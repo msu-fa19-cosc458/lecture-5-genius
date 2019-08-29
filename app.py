a09c02f41a9f1b681658ecf17672b73183a53e95
Phil B <philanaaurelia@gmail.com>
2019-08-29 14:06:34 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+# api_test.py
+import requests
+
+url = \ "https://api.genius.com/search?q=Kendrick%20Lamar"
+
+
+my_headers = {
+    "Authorization": "Bearer qiICws1IclXFRZmTwrTJI7k4m8vWoOwy2smTaAmQ2RuyiWztfrPPijT9ea2i2nA-"
+}
+
+response = requests.get(url, headers=my_headers)
+print(response.text)
